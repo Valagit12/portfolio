38e1adacdd798496ffa974f88ca1ab7ee12a9a61
Valareza Arezehgar <valareza.arezehgar@gmail.com>
2025-07-04 18:25:15 -0400

    fixing output

---
+++ build.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+const distDir = path.join(__dirname, 'dist');
+const publicDir = path.join(__dirname, 'public');
+const viewsDir = path.join(__dirname, 'views');
+
+fs.rmSync(distDir, { recursive: true, force: true });
+fs.mkdirSync(distDir, { recursive: true });
+
+ejs.renderFile(
+  path.join(viewsDir, 'index.ejs'),
+  (err, html) => {
+    if (err) throw err;
+    fs.writeFileSync(path.join(distDir, 'index.html'), html);
+  }
+);
+
+function copyRecursive(src, dest) {
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+  fs.mkdirSync(dest, { recursive: true });
+
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyRecursive(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyRecursive(publicDir, distDir);
